9d0ff1b71185a5b7cad6f82a80ad1fa90286104d
Aaron <mohammad.chegini@hotmail.com>
2019-09-08 18:11:19 +0430

    07 Templating engine configured

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const exphbs = require('express-handlebars');
+const members = require('./members');
 
 //const logger = require('./middleware/logger')
 // Initializing Middlewares
 // app.use(logger);
 
+// Handlebar Middleware
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
 // Body Parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Rendering
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: "My App!",
+        members
+    })
+})
 
 // Setting static folder
 app.use(express.static(path.join(__dirname, 'public')));
